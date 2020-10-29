7f6a45beacea9d630e88ad329c5cd4c000f1ba08
Menachem Neiman <Nerevar123@gmail.com>
2020-10-29 02:20:05 +0200

    fix: fixed const name

--- routes/cards.js
+++ routes/cards.js
@@ -2,9 +2,9 @@ const getCards = require('express').Router();
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const usersPath = path.join(__dirname, '../data/cards.json');
+const cardsPath = path.join(__dirname, '../data/cards.json');
 
-fsPromises.readFile(usersPath, { encoding: 'utf8' })
+fsPromises.readFile(cardsPath, { encoding: 'utf8' })
   .then((data) => JSON.parse(data))
   .then((cards) => {
     getCards.get('/', (req, res) => {
